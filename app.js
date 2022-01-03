67967a57c972d6ffab4bb2ada5ffce9a98cfc788
antash-mishra <mishraantash34@gmail.com>
2022-01-03 11:48:33 +0530

    app changed

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(session({
 }));
 
 const dbService = require('./dbService');
+//const { delete } = require('./routes/auth');
 
 app.use(cors());
 app.use(express.json());
@@ -54,24 +55,54 @@ app.use('/add', require('./routes/auth.js'));
 // });
 
 app.get('/moviesList', (req,res, error) => {
-    db.query("select title,genre from Movies where movie_id in (select movie_id from user_watchlist_movie where username = ?)",[req.session.username], (err, result) => {
-        if (err) throw err;
+    db.query("select title,genre,movie_id from Movies where movie_id in (select movie_id from user_watchlist_movie where username = ?)",[req.session.username], (err,result) => {
+        console.log(result);
+        res.send(result);
+    });
+
+});
+
+app.get('/sereisList', (req,res, error) => {
+    db.query("select title,genre,series_id from Series where series_id in (select series_id from user_watchlist_series where username = ?)",[req.session.username], (err, result) => {
+        if(err) {
+            console.log(err);
+        }
+        console.log('series');
         console.log(result);
         res.send(result);
     });
 });
 
-app.get('/sereisList', (req,res, error) => {
-    db.query("select title,genre from Series where series_id in (select series_id from user_watchlist_series where username = ?)",[req.session.username], (err, result) => {
-        if (err) throw err;
+app.delete('/moviesList/:id', (req, res, error) => {
+    console.log('hello, hi');
+    console.log(res);
+    const { id } = req.params;
+    db.query("delete from user_watchlist_movie where username = ? and movie_id = ?;", [req.session.username, id ], (err, result) => {
+        if(err) {
+            console.log(err);
+        }
+        console.log('delete');
         console.log(result);
-        res.send(result);
+        res.send('deleted');
+    });
+});
+
+app.delete('/sereisList/:id', (req, res, error) => {
+    console.log('hello, hi');
+    console.log(res);
+    const { id } = req.params;
+    db.query("delete from user_watchlist_series where username = ? and series_id = ?;", [req.session.username, id ], (err, result) => {
+        if(err) {
+            console.log(err);
+        }
+        console.log('delete');
+        console.log(result);
+        res.send('deleted');
     });
 });
 
 app.get('/watchedMovieList', (req,res, error) => {
     db.query("SELECT m.title, m.genre, w.date from Movies m, watched_movie w WHERE w.movie_id = m.movie_id AND w.username = ?",[req.session.username], (err, result) => {
-        if (err) throw err;
         console.log(result);
         res.send(result);
     });
@@ -79,7 +110,6 @@ app.get('/watchedMovieList', (req,res, error) => {
 
 app.get('/watchedSeriesList', (req,res, error) => {
     db.query("SELECT m.title, m.genre, w.date, w.season from Series m, watched_series w WHERE w.series_id = m.series_id AND w.username = ?",[req.session.username], (err, result) => {
-        if (err) throw err;
         console.log(result);
         res.send(result);
     });
@@ -87,7 +117,6 @@ app.get('/watchedSeriesList', (req,res, error) => {
 
 app.get('/seriesReview', (req,res, error) => {
     db.query("call procedure_review_series()", (err, result) => {
-        if (err) throw err;
         console.log(result);
         res.send(result);
     });
@@ -95,7 +124,6 @@ app.get('/seriesReview', (req,res, error) => {
 
 app.get('/movieReview', (req,res, error) => {
     db.query("call procedure_review_movie()", (err, result) => {
-        if (err) throw err;
         console.log('review movies');
         console.log(result);
         res.send(result);
